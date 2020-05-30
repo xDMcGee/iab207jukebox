b4647d14a1c85f8af2dd924d370162e00fe6743c
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-05-30 21:21:42 +1000

    Trying to implement rough href routing

--- marketplace/views.py
+++ marketplace/views.py
@@ -6,3 +6,19 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template("index.html")
+
+@bp.route('/item_create')
+def item_create():
+    return render_template("item_create.html")
+
+@bp.route('/item_details')
+def item_details():
+    return render_template("item_details.html")
+
+@bp.route('/item_list')
+def item_list():
+    return render_template("item_list.html")
+
+@bp.route('/item_order')
+def item_order():
+    return render_template("item_order.html")
