ee6b8df8ad995c805c9d09e734fcb030ad4412ea
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-01 14:07:27 +1000

    Update models.py

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import DateTime
 
 class User(db.Model):
     __tablename__='users'
@@ -11,3 +12,10 @@ class User(db.Model):
 
     def __repr__(self):
         return "<Name: {}, id: {}>".format(self.name, self.id)
+
+class Comment(db.model):
+    comment_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    comment_text = db.Column(db.String(400))
+    create_at = db.Column(db.DateTime, default=datetime.now())
+    
