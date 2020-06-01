641dd23d5713d6af2715205f3e18aedb0d34a679
Alexis Csanyi-Vo <n10479368@qut.edu.au>
2020-06-01 14:59:27 +1000

    Error 404 functions in init

--- marketplace/views.py
+++ marketplace/views.py
@@ -45,10 +45,3 @@ def register():
 
 
 
-@bp.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-@bp.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500 
