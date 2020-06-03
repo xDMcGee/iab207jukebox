77b7866daae9b34fc4a43124150b5eb6b764be7f
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-03 19:06:35 +1000

    More db and Index stuffs

--- marketplace/views.py
+++ marketplace/views.py
@@ -16,7 +16,7 @@ def index():
     db.session.add(new_product)
     db.session.commit()
 
-    vinyls = Product.query.filter(Product.category == "Vinyl")
+    vinyls = Product.query.filter(Product.category == ProductType.vinyl)
     print(vinyls)
     accessories = Product.query.filter(Product.category == "Accessory")
     players = Product.query.filter(Product.category == "Player")
