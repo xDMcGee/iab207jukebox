fa96baf60380652abd4c5abe3cf8f9b79f6d7fde
Alexis Csanyi-Vo <n10479368@qut.edu.au>
2020-06-04 12:27:47 +1000

    trying to fix navbar

--- marketplace/views.py
+++ marketplace/views.py
@@ -28,17 +28,17 @@ def item_create():
 def item_details():
     return render_template("item_details.html")
 
-@bp.route('/item_list/vinyls')
+@bp.route('/vinyls')
 def item_list_vinyl():
     prodlist = Product.query.filter_by(category = ProductType.vinyl).all()
     return render_template("item_list.html", prodlist=prodlist)
 
-@bp.route('/item_list/accessories')
+@bp.route('/accessories')
 def item_list_acc():
     prodlist = Product.query.filter_by(category = ProductType.accessory).all()
     return render_template("item_list.html", prodlist=prodlist)
 
-@bp.route('/item_list/tables')
+@bp.route('/tables')
 def item_list_tables():
     prodlist = Product.query.filter_by(category = ProductType.player).all()
     return render_template("item_list.html", prodlist=prodlist)
