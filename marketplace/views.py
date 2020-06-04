bb66b8db56fc65075583802a9b4773b1d8ad9de7
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-04 15:15:06 +1000

    Trying to fix broken view routing

--- marketplace/views.py
+++ marketplace/views.py
@@ -33,7 +33,7 @@ def item_details():
 def item_list():
     type = request.args.get('type')
     if not (type is None):
-        prodlist = Product.query.filter_by(category = type).all()
+        prodlist = Product.query.filter_by(category = ProductType[str(type)]).all()
     else:
         prodlist = Product.query.all()
     return render_template("item_list.html", prodlist=prodlist)
