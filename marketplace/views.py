5922e85d1c074a50c74b51ea3cf7e2800e872e1c
Alexis Csanyi-Vo <n10479368@qut.edu.au>
2020-06-08 12:49:18 +1000

    user create db implementation

--- marketplace/views.py
+++ marketplace/views.py
@@ -69,7 +69,7 @@ def register():
         user_add = User(name=register_form.user_name.data,
                         password_hash=register_form.confirm.data,
                         email_id=register_form.email_id.data,
-                        user_type=register_form.account_type.data)
+                        user_type='Buyer')
         db.session.add(user_add)
         db.session.commit()
         print('Successfully created account!', 'Success')
