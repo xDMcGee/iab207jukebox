44111099e3c6d1293911c2d131f37afb78aa3f14
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-08 13:46:45 +1000

    Update products.py

--- marketplace/products.py
+++ marketplace/products.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .models import Product, Comment
+from .forms  import ProductForm
 
 bp = Blueprint('product', __name__, url_prefix='/products')
 
@@ -8,5 +9,14 @@ def item_details(id):
     product = get_product()
     return render_template('item_details.html', product=product)
 
-def get_product():
-    return product
+@bp.route('/create', methods=['GET','POST'])
+def create():
+    print('Method type', request.method)
+    form = ProductForm()
+    if form.validate_on_submit():
+        print('Succesfully create new product', 'success')
+
+    return render_template('item_create.html', form=form)
+
+# def get_product():
+#     return product
