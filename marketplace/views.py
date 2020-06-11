579dc5277dda0b00a8860d55dce577cf9bc6cb1c
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-11 17:59:43 +1000

    Attempting to add searching

--- marketplace/views.py
+++ marketplace/views.py
@@ -18,16 +18,17 @@ def index():
 
     # if session['user'] == None:
     session['logged'] = 0
-    search()
+    #search()
 
     return render_template("index.html", vinyls=vinyls, accessories=accessories, players=players, users=users)
 
+@bp.route('/', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
     if form.validate_on_submit():
         search_input = form.query.data
         print(search_input)
-    return render_template("search_bar.html", searchForm = form)
+    return render_template("./components/search_bar.html", searchForm = form)
 
 @bp.route('/item_create', methods=['GET', 'POST'])
 def item_create():
