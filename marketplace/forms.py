a94569b08fd50ae3048fdf0e5ca89f3288af1120
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-11 12:03:52 +1000

    Update forms.py

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, IntegerField, MultipleFileField, SelectField, RadioField, FileField
+from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, IntegerField, MultipleFileField, SelectField, RadioField, FileField, FileRequired
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 from .models import ProductType, SubTypes
