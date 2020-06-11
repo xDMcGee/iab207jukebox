f1649220632c46c409744690600794c5b927536e
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-11 13:27:54 +1000

    Fleshing out item creation

--- marketplace/products.py
+++ marketplace/products.py
@@ -31,7 +31,7 @@ def create():
         product = Product(album_title = form.album_title.data,
         artist_name = form.artist_name.data,
         category = ProductType[cat],
-        subcategory = SubTypes[form.product_sub_type.data].value,
+        subcategory = SubTypes[form.product_sub_type.data],
         price = form.price.data,
         stock = form.stock.data,
         description = form.description.data,
