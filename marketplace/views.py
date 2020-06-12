f0ccd953a5b8409286333ecf4f31b0cd63315820
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-12 14:39:36 +1000

    Revert "Update views.py"
    This reverts commit 0b179ad988737b4351e5d1acfe58e2d9b1316d8f.

--- marketplace/views.py
+++ marketplace/views.py
@@ -69,6 +69,46 @@ def item_list():
 def item_order():
     return render_template("item_order.html")
 
+@bp.route('/login', methods=['GET', 'POST'])
+def authenticate(): #view function
+    print('In Login View function')
+    login_form = LoginForm()
+    error=None
+    if(login_form.validate_on_submit()==True):
+        user_name = login_form.user_name.data
+        password = login_form.password.data
+        u1 = User.query.filter_by(name=user_name).first()
+        if u1 is None:
+            error='Incorrect user name'
+        elif not check_password_hash(u1.password_hash, password): # takes the hash and password
+            error='Incorrect password'
+        if error is None:
+            login_user(u1)
+            nextp = request.args.get('next') #this gives the url from where the login page was accessed
+            print(nextp)
+            if next is None or not nextp.startswith('/'):
+                return redirect(url_for('index'))
+            return redirect(nextp)
+        else:
+            flash(error)
+    return render_template('user.html', form=login_form, heading='Login')
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = RegisterForm()
+    print('Method Type: ', request.method)
+    if register_form.validate_on_submit():
+        user_add = User(name=register_form.user_name.data,
+                        password_hash=generate_password_hash(register_form.confirm.data, salt_length=16),
+                        email_id=register_form.email_id.data,
+                        user_type=register_form.account_type.data)
+        db.session.add(user_add)
+        db.session.commit()
+        print('Successfully created account!', 'Success')
+        #return redirect(url_for('user'))
+
+    return render_template("user.html", form=register_form)
+
 @bp.route('/logout')
 def logout():
     if 'email' in session:
