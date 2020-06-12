217aba21e55f43a241c2c425a11015e67a87ff8f
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-12 14:35:18 +1000

    Update __init__.py

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -45,7 +45,7 @@ def create_app():
     login_manager.init_app(app)
 
     #create a user loader function takes userid and returns User
-    #from .models import User  # importing here to avoid circular references
+    from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
