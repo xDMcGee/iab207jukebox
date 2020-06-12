2d60e0dca805660bd253e450bc61508080d1c44e
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-12 14:38:23 +1000

    Revert "Update auth.py"
    This reverts commit 1e56cc9dc3be6f2dc9145e267d30c5321d9158c3.

--- marketplace/auth.py
+++ marketplace/auth.py
@@ -38,46 +38,21 @@ def authenticate(): #view function
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
 
-# @bp.route('/register', methods=['GET', 'POST'])
-# def register():
-#     register_form = RegisterForm()
-#     print('Method Type: ', request.method)
-#     if register_form.validate_on_submit():
-#         user_add = User(name=register_form.user_name.data,
-#                         password_hash=generate_password_hash(register_form.confirm.data, salt_length=16),
-#                         email_id=register_form.email_id.data,
-#                         user_type=register_form.account_type.data)
-#         db.session.add(user_add)
-#         db.session.commit()
-#         print('Successfully created account!', 'Success')
-#         #return redirect(url_for('user'))
-
-#     return render_template("user.html", form=register_form)
-
-@bp.route('/register', methods=['GET','POST'])
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
-    register = RegisterForm()
-    if (register.validate_on_submit() == True):
-        user_name = register.user_name.data
-        password = register.password.data
-        email = register.email_id.data
-        user_type = register.account_type.data
-
-        u1 = User.query.filter_by(name=user_name).first()
-        if u1:
-            flash('User name already exist, please login')
-            return redirect(url_for('auth.login'))
-        password = generate_password_hash(password)
-        new_user = User(name=user_name, password_hash=password, email_id = email, user_type = user_type)
-        db.session.add(new_user)
+    register_form = RegisterForm()
+    print('Method Type: ', request.method)
+    if register_form.validate_on_submit():
+        user_add = User(name=register_form.user_name.data,
+                        password_hash=generate_password_hash(register_form.confirm.data, salt_length=16),
+                        email_id=register_form.email_id.data,
+                        user_type=register_form.account_type.data)
+        db.session.add(user_add)
         db.session.commit()
+        print('Successfully created account!', 'Success')
+        #return redirect(url_for('user'))
 
-        return redirect(url_for('index.html'))
-
-    else:
-        return render_template('user.html', form = register, heading='Register')
-
-
+    return render_template("user.html", form=register_form)
 
 # this is the hint for a login function
 # @bp.route('/login', methods=['GET', 'POST'])
