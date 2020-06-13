f2d37a2709ce0ef32a672d0692443ddb6f661c67
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-13 17:25:45 +1000

    Update products.py

--- marketplace/products.py
+++ marketplace/products.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, jsonif
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from .models import Product, Comment, ProductType, SubTypes
-from .forms  import ProductForm, CommentForm
+from .forms import ProductForm, CommentForm, OrderForm
 from . import db
 from flask_login import current_user
 
@@ -10,35 +10,38 @@ import os
 
 bp = Blueprint('product', __name__, url_prefix='/products')
 
+
 @bp.route('/<id>')
 def show(id):
     product = Product.query.filter_by(id=id).first()
     if product is None:
         abort(404)
 
-    #Reformatting the date to be user-readable
+    # Reformatting the date to be user-readable
     created_date = product.created_date.strftime('%d/%m/%Y')
     cform = CommentForm()
-    return render_template('show.html', product = product, created_date = created_date, form = cform)
+    return render_template('show.html', product=product, created_date=created_date, form=cform)
 
-@bp.route('/<id>/comment', methods=['GET','POST'])
+
+@bp.route('/<id>/comment', methods=['GET', 'POST'])
 def comment(id):
     product = Product.query.filter_by(id=id).first()
     cform = CommentForm()
     if cform.validate_on_submit():
         comment = Comment(
-            user_name = current_user.name,
+            user_name=current_user.name,
             text=cform.text.data,
-            product_id = product.id,
-            user_id = current_user.id
+            product_id=product.id,
+            user_id=current_user.id
         )
 
         db.session.add(comment)
         db.session.commit()
         print('Your comment has been added', 'success')
-    return redirect(url_for('.show', id = id))
+    return redirect(url_for('.show', id=id))
 
-@bp.route('/create', methods=['GET','POST'])
+
+@bp.route('/create', methods=['GET', 'POST'])
 def create():
     if not current_user.is_authenticated:
         return current_app.login_manager.unauthorized()
@@ -64,19 +67,20 @@ def create():
         dir_path = os.path.join(BASE_PATH, 'static/img/' + str(oldId + 1))
         os.makedirs(dir_path, exist_ok=True)
 
-        upload_path = os.path.join(BASE_PATH, 'static/img/' + str(oldId + 1), secure_filename(filename))
+        upload_path = os.path.join(
+            BASE_PATH, 'static/img/' + str(oldId + 1), secure_filename(filename))
         img_file.save(upload_path)
 
         product = Product(
-        item_name = form.item_name.data,
-        item_manufacturer = form.item_manufacturer.data,
-        price = form.price.data,
-        stock = form.stock.data,
-        description = form.description.data,
-        category = ProductType[cat],
-        subcategory = SubTypes[form.product_sub_type.data],
-        image = filename,
-        seller_id = 1)
+            item_name=form.item_name.data,
+            item_manufacturer=form.item_manufacturer.data,
+            price=form.price.data,
+            stock=form.stock.data,
+            description=form.description.data,
+            category=ProductType[cat],
+            subcategory=SubTypes[form.product_sub_type.data],
+            image=filename,
+            seller_id=1)
 
         db.session.add(product)
         db.session.commit()
@@ -86,12 +90,28 @@ def create():
 
     return render_template('components/create_product.html', form=form)
 
+
 @bp.route('/_get_subtypes/')
 def _get_subtypes():
     product_type = request.args.get('pt', 0, type=int)
     sub_type = SubTypes.specchoice(SubTypes, product_type)
     return jsonify(sub_type)
 
-@bp.route('/order')
+
+@bp.route('/order', methods=['GET', 'POST'])
 def order():
+    order_form = OrderForm()
+
+    if order_form.validate_on_submit():
+        order = Order(
+            address=order_form.address.data,
+            quantity=order_form.quantity.data
+        )
+
+        if quantity > Product.stock:
+            print('Cannot purchase more than the available stock')
+            return redirect(url_for('product.order'))
+        # else:
+        #     print('Order has been placed with order ID:', order_id)
+        #     return redirect(url_for())
     return render_template("item_order.html")
