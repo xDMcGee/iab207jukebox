664661912970723f340257e944917cd86b53010f
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-14 19:48:46 +1000

    Update forms.py

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -25,7 +25,7 @@ def length_check(checkType = 'BSB', reqLength = 6):
 class RegisterForm(FlaskForm):
     user_name = StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    phone_number = StringField('Phone number', validators=[InputRequired(), length_check(checkType='Phone number', reqLength=10), Regexp('\d', message=_('This is not an integer number, please see the example and try again'))]
+    phone_number = StringField('Phone number', validators=[InputRequired(), length_check(checkType='Phone number', reqLength=10), Regexp('\d', message='This is not an integer number, please see the example and try again')]
 
     #linking two fields - password should be equal to data entered in confirm
     password = PasswordField("Password", validators=[InputRequired(),
