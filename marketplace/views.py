0505798c88800141acf339791fc95d6a68ec33a3
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-14 20:23:22 +1000

    Moved all auth routes to separate file

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,14 +1,12 @@
-from flask import Blueprint, render_template, session, request, redirect, url_for, flash
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user
+from sqlalchemy import or_, and_
+
 from . import db
 from .models import Product, ProductType, User, SubTypes
 from .forms import LoginForm, RegisterForm, ProductForm, FilterForm
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import login_user, login_required, logout_user
-from sqlalchemy import or_, and_
-# from products import show, create
-bp = Blueprint('main', __name__)
 
+bp = Blueprint('main', __name__)
 
 @bp.route('/search', methods=['GET', 'POST'])
 def search():
@@ -21,9 +19,6 @@ def index():
     vinyls = Product.query.filter_by(category=ProductType['Vinyl']).limit(6).all()
     accessories = Product.query.filter_by(category=ProductType['Accessory']).limit(6).all()
     players = Product.query.filter_by(category=ProductType['Player']).limit(6).all()
-
-    # if session['user'] == None:
-    session['logged'] = 0
 
     return render_template("index.html", vinyls=vinyls, accessories=accessories, players=players, users=users)
 
@@ -47,87 +42,3 @@ def item_list():
     else:
         prodlist = Product.query.all()
     return render_template("item_list.html", prodlist = prodlist, arg = None)
-
-@bp.route('/login', methods=['GET', 'POST'])
-def authenticate(): #view function
-    print('In Login View function')
-    login_form = LoginForm()
-    error=None
-    if(login_form.validate_on_submit()==True):
-        user_name = login_form.user_name.data
-        password = login_form.password.data
-        u1 = User.query.filter_by(name=user_name).first()
-        if u1 is None:
-            error='Incorrect user name'
-        elif not check_password_hash(u1.password_hash, password): # takes the hash and password
-            error='Incorrect password'
-        if error is None:
-            login_user(u1)
-            nextp = request.args.get('next') #this gives the url from where the login page was accessed
-            print(nextp)
-            if nextp is None or not nextp.startswith('/'):
-                return redirect(url_for('main.index'))
-            return redirect(nextp)
-        else:
-            flash(error)
-    return render_template('user.html', form=login_form, heading='Login')
-
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    register_form = RegisterForm()
-    print('Method Type: ', request.method)
-    if register_form.validate_on_submit():
-
-        uTest = User.query.filter(or_(User.name == register_form.user_name.data, User.email_id == register_form.email_id.data, User.phone_number == register_form.phone_number.data)).first()
-        if uTest:
-            flash('A User with this information already exists, please try again')
-            return redirect(url_for('main.register'))
-
-        # while True:
-        #     try:
-        #         register_form.phone_number = int(register_form.phone_number)
-        #     except ValueError:
-        #         flash('Please input 10 digit number')
-        #         return redirect(url_for('main.register'))
-
-
-        # def phone_number_input(register_form.phone_number):
-        #     while True:
-        #         try:
-        #             userInput = int(register_form.phone_number)
-        #         except ValueError:
-        #             print("Not an integer! Try again")
-        #             return redirect(url_for('main.register'))
-        #         else: 
-        #             return userInput
-
-        if (register_form.account_type.data == "Buyer"):
-            bsb = None
-            account_no = None
-        else:
-            bsb = register_form.bsb.data
-            account_no = register_form.account_no.data
-            accTest = User.query.filter(or_(User.bsb == str(register_form.bsb.data), User.account_no == str(register_form.account_no.data))).first()
-            if accTest:
-                flash('A User with this information already exists, please try again')
-                return redirect(url_for('main.register'))
-
-        user_add = User(name=register_form.user_name.data,
-                        password_hash=generate_password_hash(register_form.confirm.data, salt_length=16),
-                        email_id=register_form.email_id.data,
-                        user_type=register_form.account_type.data,
-                        bsb=bsb,
-                        account_no=account_no,
-                        phone_number=register_form.phone_number.data)
-
-        db.session.add(user_add)
-        db.session.commit()
-        print('Successfully created account!', 'Success')
-        return redirect(url_for('main.authenticate'))
-
-    return render_template("user.html", form=register_form, heading='Register')
-
-@bp.route('/signout')
-def signout():
-    logout_user()
-    return 'Logout Successful'
