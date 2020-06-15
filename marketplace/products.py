002f172173ae58c902350154497a956f51a148d9
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-15 14:12:55 +1000

    poop

--- marketplace/products.py
+++ marketplace/products.py
@@ -144,7 +144,7 @@ def order(id):
     if order_form.validate_on_submit():
         if order_form.quantity.data > product.stock:
             print('Cannot purchase more than the available stock')
-            return redirect(url_for('product.order'))
+            return redirect(url_for('product.order', id = id))
 
         if order_form.validate_on_submit():
             order = Order(
