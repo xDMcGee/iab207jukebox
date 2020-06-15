76d9b5a5fea9a577a58c8f29c29ab8d6ffe5ae56
Alexis Csanyi-Vo <n10479368@qut.edu.au>
2020-06-15 17:20:17 +1000

    ordlist

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, login_required, logout_user, current_user, current_app
+from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy import or_, and_
 
 #Custom Imports
