1387262e29b6c0f64176580ab7a6b2f105860fb5
timothy218 <66167340+timothy218@users.noreply.github.com>
2020-06-15 12:21:16 +1000

    Update forms.py

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, IntegerField, SelectField, RadioField, DateField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, IntegerField, MultipleFileField, SelectField, RadioField, DateField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import InputRequired, Length, Email, EqualTo, Optional, ValidationError, Regexp
 
 from .models import ProductType, SubTypes
 
-#Function to valid length of inputs
+# creates the login information
+
+
+class LoginForm(FlaskForm):
+    user_name = StringField("User Name", validators=[
+                            InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[
+                             InputRequired('Enter user password')])
+    submit = SubmitField("Login")
+
+
 def length_check(checkType='BSB', reqLength=6):
     message = 'Please enter a valid %d digit %s number' % (
         reqLength, checkType)
@@ -17,60 +27,73 @@ def length_check(checkType='BSB', reqLength=6):
 
     return _length
 
-# creates the login information
-class LoginForm(FlaskForm):
-    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
-    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
-    submit = SubmitField("Login")
-
 # this is the registration form
+
+
 class RegisterForm(FlaskForm):
     user_name = StringField("User Name", validators=[InputRequired()])
-
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    phone_number = StringField('Phone number', validators=[InputRequired(), length_check(checkType = 'Phone number', reqLength = 10), Regexp('^[0-9]+$', message='Please only use valid numbers 0-9')])
+    email_id = StringField("Email Address", validators=[
+                           Email("Please enter a valid email")])
+    phone_number = StringField('Phone number', validators=[InputRequired(),
+                                                length_check(checkType='Phone', reqLength=10),
+                                                Regexp('\d', message='Please input number')])
 
     # linking two fields - password should be equal to data entered in confirm
-    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message = "Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+    password=PasswordField("Password", validators=[InputRequired(),
+                  EqualTo('confirm', message="Passwords should match")])
+    confirm=PasswordField("Confirm Password")
 
     # Select what user account to use
-    account_type = SelectField('Account Type', choices=[('Buyer', 'Buyer'), ('Seller', 'Seller')], validate_choice=False, id="select_user_type")
-    bsb = IntegerField("BSB", validators=[Optional(), length_check()], id="bsb_input")
-    account_no = IntegerField("Account Number", validators=[Optional(), length_check(checkType = 'Account', reqLength = 9)], id="account_no_input")
+    account_type=SelectField('Account Type', choices=[(
+        'Buyer', 'Buyer'), ('Seller', 'Seller')], validate_choice=False, id="select_user_type")
+    bsb=IntegerField("BSB", validators=[
+                     Optional(), length_check()], id="bsb_input")
+    account_no=IntegerField("Account Number", validators=[Optional(), length_check(
+        checkType='Account', reqLength=9)], id="account_no_input")
 
     # submit button
-    submit = SubmitField("Register")
+    submit=SubmitField("Register")
 
 class ProductForm(FlaskForm):
 
     type_choices=ProductType.choices()
     for choice in range(len(type_choices)):
         type_choices[choice]=type_choices[choice][::-1]
-
-    product_type=SelectField("Product Type", choices=type_choices, validate_choice=False, id="select_product_type")
+    product_type=SelectField("Product Type", choices=type_choices,
+                             validate_choice=False, id="select_product_type")
 
     sub_type_choices=SubTypes.fullchoices(SubTypes)
-    product_sub_type=SelectField("Product Sub Type", choices=sub_type_choices, validate_choice=False, id="select_sub_type")
+    product_sub_type=SelectField(
+        "Product Sub Type", choices=sub_type_choices, validate_choice=False, id="select_sub_type")
 
     item_name=StringField('Product name', validators=[InputRequired()])
-    item_manufacturer=StringField('Product Manufacturer', validators=[InputRequired()])
+    item_manufacturer=StringField(
+        'Product Manufacturer', validators=[InputRequired()])
 
     price=IntegerField('Item price', validators=[InputRequired()])
     stock=IntegerField('Number of stock', validators=[InputRequired()])
-    description=TextAreaField('Description of product', validators=[InputRequired()])
-    image=FileField('Image', validators=[FileRequired(), FileAllowed({'jpg', 'png'}, message='Images only!')])
-
+    description=TextAreaField('Description of product',
+                              validators=[InputRequired()])
+    image=FileField('Image', validators=[
+        FileRequired(),
+        FileAllowed({'jpg', 'png'}, message='Images only!')])
+    # image = MultipleFileField('Image of the product', [validators.regexp('^[^/\\]\.jpg$')])
     submit=SubmitField('Create')
 
 class FilterForm(FlaskForm):
     sub_type_choices=SubTypes.fullchoices(SubTypes)
-    product_sub_type=SelectField("", choices=sub_type_choices, validate_choice=False, id="select_sub_type")
+    product_sub_type=SelectField(
+        "", choices=sub_type_choices, validate_choice=False, id="select_sub_type")
 
 class CommentForm(FlaskForm):
     text=TextAreaField('Comment', validators=[InputRequired()])
     submit=SubmitField('Post')
 
 class OrderForm(FlaskForm):
-    address=StringField('Delivery Addresss', validators=[InputRequired()])
+    street_address=StringField('Street Address', validators=[InputRequired()])
+    street_address2=StringField('Street Address 2', validators=[InputRequired()])
+    city=StringField('City', validators=[InputRequired()])
+    state=StringField('State/Province', validators=[InputRequired()])
+    postcode=IntegerField('Postcode', validators=[InputRequired(),
+                                                length_check(checkType='Postcode', reqLength=4))
     quantity=IntegerField('Quantity', validators=[InputRequired()])
