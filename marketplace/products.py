c79bb5c889e450ec1f7b1826baa056358ba48d4d
Adam Kahwati <adam.kahwati@connect.qut.edu.au>
2020-06-15 15:31:41 +1000

    flashing maybe breaking everything

--- marketplace/products.py
+++ marketplace/products.py
@@ -143,8 +143,7 @@ def order(id):
 
     if order_form.validate_on_submit():
         if order_form.quantity.data > product.stock:
-            flash('Cannot purchase more than the available stock')
-            return redirect(url_for('product.order', id = id))
+            return flash('Cannot purchase more than the available stock')
 
         if order_form.validate_on_submit():
             order = Order(
